e36b6ffb6ba3cbbf397599fa15cf64c35ce0cc63
rkoul <rajiv_koul2000@yahoo.com>
2015-01-06 10:32:20 -0500

    New Code 01/06/2015

--- features/Test.feature
+++ features/Test.feature
@@ -6,14 +6,15 @@ Scenario: Check the Dominos home page
  When I visit the Dominos home page
  Then I should see the Dominos home page
 
-@location
+@OrderPizza
 Scenario: Location Search for Dominos Pizza
   Given I visit the Dominos home page
   When I click the "Order Online" tab
   Then I should see the Dominos Location Search page
   When I fill the Address Information form
-  Then I should see the Entrees page  
+  And  I click on the Speciality Pizza
+  And  I click on the Order link 
+  And  I click on Add to Order link
+  And  I click on Checkout link
+  And  I click on Continue Checkout link
 
- 
-
- 
--- features/step_definitions/Test.step.rb
+++ features/step_definitions/Test.step.rb
@@ -4,12 +4,12 @@ end
 
 Then /^I should see the Dominos home page$/ do
    expect(page).to have_selector(:id, 'homePage')
-   sleep 10
+   sleep 5
 end
 
 When(/^I click the "(.*?)" tab$/) do |tab|
   click_link('Order Online')
-  sleep 10
+  sleep 5
 end
  
 Then(/^I should see the Dominos Location Search page$/) do
@@ -18,16 +18,35 @@ end
 
 When(/^I fill the Address Information form$/) do
 	select('House', :from => 'Address_Type')
-	fill_in('Street', :with => '25199 Larks Terrace')
+	fill_in('Street', :with => 'Larks Terrace')
     fill_in('City', :with => 'Southriding')
     select('VA', :from => 'Region')
     fill_in('Postal_Code', :with => '20152')
     click_button('Continue')
-    sleep 10
+    sleep 5
 end
 
-Then(/^I should see the Entrees Page$/) do
-   expect(page).to have_selector(:id, 'entreesPage')
-   sleep 10
+And(/^I click on the Speciality Pizza$/) do
+   find('.navigation-Pizza').click
+   sleep 5
 end
 
+And(/^I click on the Order link$/) do
+  find(:xpath, '/html/body/div[2]/div[1]/div/div/section[1]/div/div[2]/a').click
+  sleep 5
+end
+
+And(/^I click on Add to Order link$/) do 
+  click_button('Add to Order')
+  sleep 5
+end
+
+And(/^I click on Checkout link$/) do
+  find(:xpath, '/html/body/div[2]/div[1]/aside/div[1]/div/div[2]/div[1]/a/span').click
+  sleep 5
+end
+
+And(/^I click on Continue Checkout link$/) do
+  find(:xpath, '/html/body/div[2]/div[2]/div/div/div/aside/a').click
+  sleep 5
+end
